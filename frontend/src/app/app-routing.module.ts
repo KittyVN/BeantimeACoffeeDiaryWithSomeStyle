0f84bf9df26b285af88a9424a97471460eb708f9
Heinrich Peukert <e11809929@student.tuwien.ac.at>
2022-12-05 17:51:52 +0100

    (protected-routes): adding role-protected admin route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { RoleGuard } from 'src/services/auth/role.guard';
 
 import { RegisterComponent } from './pages/register/register.component';
 import { TestHomeComponent } from './pages/test-home/test-home.component';
 import { AuthGuard } from '../services/auth/auth.guard';
+import { TestAdminPageComponent } from './pages/test-admin-page/test-admin-page.component';
 
 import type { Routes } from '@angular/router';
 
@@ -12,6 +14,14 @@ const routes: Routes = [
     path: 'home',
     component: TestHomeComponent,
     canActivate: [AuthGuard],
+  },
+  {
+    path: 'admin',
+    component: TestAdminPageComponent,
+    canActivate: [RoleGuard],
+    data: {
+      expectedRole: 'ROLE_ADMIN',
+    },
   },
   { path: 'register', component: RegisterComponent },
   { path: '**', redirectTo: 'home' },
