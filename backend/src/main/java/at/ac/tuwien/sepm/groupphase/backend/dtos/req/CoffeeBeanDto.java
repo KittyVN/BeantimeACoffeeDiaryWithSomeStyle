8a581413094a625f98bb1a2722c19c456fd37139
xiaolong <jakob.novotny@gmx.at>
2022-12-06 15:53:42 +0100

    (custom-coffee-beans): added CoffeeBeanDto

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/dtos/req/CoffeeBeanDto.java
@@ -0,0 +1,6 @@
+package at.ac.tuwien.sepm.groupphase.backend.dtos.req;
+
+import at.ac.tuwien.sepm.groupphase.backend.enums.CoffeeRoast;
+
+public record CoffeeBeanDto(String name, Float price, String origin, Integer height, CoffeeRoast coffeeRoast, String description, Boolean custom) {
+}
