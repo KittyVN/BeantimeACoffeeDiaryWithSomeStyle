9fdd454b6ec2cf860ec47315c3466a366d7afcb0
Jakob Novotny <e11800752@student.tuwien.ac.at>
2022-12-06 19:38:53 +0100

    (custom-coffee-beans): refactored CoffeeBeanDto

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/dtos/req/CoffeeBeanDto.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/dtos/req/CoffeeBeanDto.java
@@ -2,5 +2,12 @@ package at.ac.tuwien.sepm.groupphase.backend.dtos.req;
 
 import at.ac.tuwien.sepm.groupphase.backend.enums.CoffeeRoast;
 
-public record CoffeeBeanDto(String name, Float price, String origin, Integer height, CoffeeRoast coffeeRoast, String description, Boolean custom) {
-}
+public record CoffeeBeanDto(
+    String name,
+    Float price,
+    String origin,
+    Integer height,
+    CoffeeRoast coffeeRoast,
+    String description,
+    Boolean custom
+) { }
