13f2d27b800ec5baee7613e514c31868c086affd
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2022-12-08 17:32:10 +0100

    (account-list) [#20] Created UserMapper

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package at.ac.tuwien.sepm.groupphase.backend.mapper;
+
+import at.ac.tuwien.sepm.groupphase.backend.dtos.req.UserDto;
+import at.ac.tuwien.sepm.groupphase.backend.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.lang.invoke.MethodHandles;
+
+@Component
+public class UserMapper {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    public UserMapper() {
+    }
+
+    public UserDto entityToDto(User user) {
+        LOGGER.debug("entityToDto({})", user);
+        return new UserDto(user.getId(), user.getEmail(), user.getRole());
+    }
+}
