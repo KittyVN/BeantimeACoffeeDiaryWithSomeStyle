80a85a6cdfe868872727dfb9a2480605d128941e
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2022-12-08 17:15:50 +0100

    (account-list) [#20] Implemented search() in UserRepositoryCustomImpl

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/impl/UserRepositoryCustomImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/impl/UserRepositoryCustomImpl.java
@@ -6,7 +6,10 @@ import at.ac.tuwien.sepm.groupphase.backend.repository.UserRepositoryCustom;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class UserRepositoryCustomImpl implements UserRepositoryCustom {
     @PersistenceContext
@@ -14,6 +17,30 @@ public class UserRepositoryCustomImpl implements UserRepositoryCustom {
 
     @Override
     public Collection<User> search(UserDto searchParameters) {
-        return null;
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<User> query = cb.createQuery(User.class);
+        Root<User> user = query.from(User.class);
+
+        Path<String> idPath = user.get("id");
+        Path<String> emailPath = user.get("email");
+        Path<String> rolePath = user.get("role");
+
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (searchParameters.getId() != null) {
+            predicates.add(cb.equal(idPath, searchParameters.getId()));
+        }
+
+        if (searchParameters.getEmail() != null) {
+            predicates.add(cb.like(emailPath, searchParameters.getEmail()));
+        }
+
+        if (searchParameters.getRole() != null) {
+            predicates.add(cb.like(rolePath, searchParameters.getRole().toString()));
+        }
+
+        query.select(user).where(cb.and(predicates.toArray(new Predicate[predicates.size()])));
+
+        return entityManager.createQuery(query).getResultList();
     }
 }
