a792f1722d6043f151ea72d7021655396ce86ea9
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2022-12-09 14:09:17 +0100

    (account-list) [#20] Created basic logic in user-list.component.ts

--- frontend/src/app/pages/user-list/user-list.component.ts
+++ frontend/src/app/pages/user-list/user-list.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { UserDto } from '../../../dtos/req/user.dto';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css'],
 })
-export class UserListComponent {}
+export class UserListComponent implements OnInit {
+  searchParameters: UserDto = {};
+  users: UserDto[] = [];
+  columnsToDisplay = ['id', 'email', 'role'];
+
+  constructor(private service: UserService) {}
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.service.search(this.searchParameters).subscribe({
+      next: data => {
+        this.users = data;
+      },
+    });
+  }
+}
