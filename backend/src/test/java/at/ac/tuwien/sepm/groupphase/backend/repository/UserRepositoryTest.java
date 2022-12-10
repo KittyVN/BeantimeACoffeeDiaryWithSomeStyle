2ac2927e554ac91d3e217a401b7fe55ddda7ebde
Patrick Cipot <patrick.cipot@gmail.com>
2022-12-10 18:27:23 +0100

    (account-management) [#21] Fixed existing Unit tests after isActive introduction

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/repository/UserRepositoryTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/repository/UserRepositoryTest.java
@@ -21,24 +21,26 @@ public class UserRepositoryTest {
 
     @Test
     public void searchWithoutParametersReturnsAllUsers() {
-        List<User> users = (List<User>) userRepository.search(new UserDto(null, null, null));
+        List<User> users = (List<User>) userRepository.search(new UserDto(null, null, null, null));
 
-        assertThat(users.size()).isGreaterThanOrEqualTo(8);
+        assertThat(users.size()).isGreaterThanOrEqualTo(10);
         assertThat(users)
-            .map(User::getEmail, User::getRole)
-            .contains(tuple("admin@email.com", UserRole.ADMIN))
-            .contains(tuple("john.doe@example.com", UserRole.ADMIN))
-            .contains(tuple("martina.musterfrau@example.com", UserRole.USER))
-            .contains(tuple("ola.nordmann@example.com", UserRole.USER))
-            .contains(tuple("tommy.atkins@example.com", UserRole.USER))
-            .contains(tuple("jane.doe@example.com", UserRole.USER))
-            .contains(tuple("jan.jansen@example.com", UserRole.USER))
-            .contains(tuple("olaf.olaf@example.com", UserRole.USER));
+            .map(User::getEmail, User::getRole, User::isActive)
+            .contains(tuple("admin@email.com", UserRole.ADMIN, true))
+            .contains(tuple("john.doe@example.com", UserRole.ADMIN, true))
+            .contains(tuple("martina.musterfrau@example.com", UserRole.USER, true))
+            .contains(tuple("ola.nordmann@example.com", UserRole.USER, true))
+            .contains(tuple("tommy.atkins@example.com", UserRole.USER, true))
+            .contains(tuple("jane.doe@example.com", UserRole.USER, true))
+            .contains(tuple("jan.jansen@example.com", UserRole.USER, true))
+            .contains(tuple("olaf.olaf@example.com", UserRole.USER, true))
+            .contains(tuple("user@example.com", UserRole.USER, false))
+            .contains(tuple("john.smith@example.com", UserRole.USER, false));
     }
 
     @Test
     public void searchForEmailLikeDoeReturnsMin2Users() {
-        List<User> users = (List<User>) userRepository.search(new UserDto(null, "doe", null));
+        List<User> users = (List<User>) userRepository.search(new UserDto(null, "doe", null, null));
 
         assertThat(users.size()).isGreaterThanOrEqualTo(2);
         assertThat(users)
@@ -49,7 +51,7 @@ public class UserRepositoryTest {
 
     @Test
     public void searchForEmailLikeDoeAndRoleAdminReturnsMin1User() {
-        List<User> users = (List<User>) userRepository.search(new UserDto(null, "doe", UserRole.ADMIN));
+        List<User> users = (List<User>) userRepository.search(new UserDto(null, "doe", UserRole.ADMIN, null));
 
         assertThat(users.size()).isGreaterThanOrEqualTo(1);
         assertThat(users)
