3c9e7d4ea666a09d79b60c19c86608b8e50d0960
Patrick Cipot <patrick.cipot@gmail.com>
2022-12-10 18:15:31 +0100

    (account-management) [#21] Added isActive attribute to UserDto

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/dtos/req/UserDto.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/dtos/req/UserDto.java
@@ -5,10 +5,11 @@ import at.ac.tuwien.sepm.groupphase.backend.enums.UserRole;
 public record UserDto(
     Long id,
     String email,
-    UserRole role
+    UserRole role,
+    Boolean isActive
 ) {
     @Override
     public String toString() {
-        return String.format("UserDto{id=%d,email='%s',role='%s'}", id, email, role);
+        return String.format("UserDto{id=%d,email='%s',role='%s',isActive='%s'}", id, email, role, isActive);
     }
 }
