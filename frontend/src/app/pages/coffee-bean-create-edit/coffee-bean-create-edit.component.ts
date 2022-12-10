c6d9288bad0eafe31ff7ef7c25efda0af3ff8c5c
Jakob Novotny <e11800752@student.tuwien.ac.at>
2022-12-10 22:31:56 +0100

    (custom-coffee-beans): added page for creating and editing a coffee bean in frontend

---
+++ frontend/src/app/pages/coffee-bean-create-edit/coffee-bean-create-edit.component.ts
@@ -0,0 +1,136 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CoffeeBeanService } from 'src/services/cofffee-bean.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CoffeeBeanDto } from 'src/dtos';
+import { Observable } from 'rxjs';
+import { Roast } from 'src/dtos/req/roast-type.enum';
+
+export enum CoffeeBeanCreateEditMode {
+  create,
+  edit,
+}
+
+@Component({
+  selector: 'app-coffee-bean-create-edit',
+  templateUrl: './coffee-bean-create-edit.component.html',
+  styleUrls: ['./coffee-bean-create-edit.component.css'],
+})
+export class CoffeeBeanCreateEditComponent {
+  constructor(
+    private coffeeBeanService: CoffeeBeanService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  id: string | null = null;
+  mode: CoffeeBeanCreateEditMode = CoffeeBeanCreateEditMode.create;
+  coffeeBeanDto: CoffeeBeanDto = {
+    name: '',
+    custom: true,
+    coffeeRoast: Roast.LIGHT,
+  };
+
+  createEditBeanForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    price: new FormControl('', [Validators.min(0)]),
+    origin: new FormControl('', Validators.maxLength(255)),
+    height: new FormControl('', [Validators.min(0)]),
+    coffeeRoast: new FormControl('LIGHT', [Validators.required]),
+    description: new FormControl('', Validators.maxLength(255)),
+  });
+
+  public get heading(): string {
+    switch (this.mode) {
+      case CoffeeBeanCreateEditMode.create:
+        return 'Create New Coffee Bean';
+      case CoffeeBeanCreateEditMode.edit:
+        return 'Edit Coffee Bean';
+      default:
+        return '?';
+    }
+  }
+
+  public get submitButtonText(): string {
+    switch (this.mode) {
+      case CoffeeBeanCreateEditMode.create:
+        return 'Create';
+      case CoffeeBeanCreateEditMode.edit:
+        return 'Edit';
+      default:
+        return '?';
+    }
+  }
+
+  private get modeActionFinished(): string {
+    switch (this.mode) {
+      case CoffeeBeanCreateEditMode.create:
+        return 'created';
+      case CoffeeBeanCreateEditMode.edit:
+        return 'edited';
+      default:
+        return '?';
+    }
+  }
+
+  ngOnInit(): void {
+    this.route.data.subscribe(data => {
+      this.mode = data['mode'];
+    });
+    console.log(this.mode);
+    if (this.mode === CoffeeBeanCreateEditMode.edit) {
+      this.route.paramMap.subscribe(paramMap => {
+        this.id = paramMap.get('id');
+      });
+      if (this.id != null) {
+        this.coffeeBeanService.getById(this.id).subscribe({
+          next: (data: CoffeeBeanDto) => {
+            this.coffeeBeanDto = data;
+          },
+          error: (error: { message: any }) => {
+            console.error('Error fetching coffee bean: ', error.message);
+          },
+        });
+      } else {
+        console.error('Error fetching coffeeBean: id cannot be null');
+      }
+    }
+  }
+
+  onSubmit() {
+    console.log(this.createEditBeanForm.value);
+    console.log(this.createEditBeanForm.valid);
+    if (this.createEditBeanForm.valid) {
+      let observable: Observable<string>;
+      switch (this.mode) {
+        case CoffeeBeanCreateEditMode.create:
+          observable = this.coffeeBeanService.create(this.coffeeBeanDto);
+          break;
+        case CoffeeBeanCreateEditMode.edit:
+          observable = this.coffeeBeanService.edit(this.coffeeBeanDto);
+          break;
+        default:
+          console.error('Unknown CoffeeBeanCreateEditMode', this.mode);
+          return;
+      }
+      observable.subscribe({
+        next: data => {
+          console.log(
+            'Coffee bean was successfully ' + this.modeActionFinished
+          );
+        },
+        error: err => {
+          console.log('Error');
+        },
+      });
+    }
+  }
+
+  getErrorMessage() {
+    if (this.createEditBeanForm.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return '';
+  }
+}
---
+++ frontend/src/services/cofffee-bean.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CoffeeBeanDto } from 'src/dtos';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class CoffeeBeanService {
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Add a new coffee bean
+   * @param coffeeBean The bean to add
+   */
+  public create(coffeeBean: CoffeeBeanDto) {
+    return this.http.post('coffee-bean/create', coffeeBean, {
+      responseType: 'text',
+    });
+  }
+
+  /**
+   * Edit an existing coffee bean
+   * @param coffeeBean The edited bean
+   */
+  public edit(coffeeBean: CoffeeBeanDto) {
+    return this.http.put('coffee-bean/edit', coffeeBean, {
+      responseType: 'text',
+    });
+  }
+
+  /**
+   * Get a coffee bean out of the data storage by its id
+   *
+   * @param id the id of the coffee bean to fetch
+   * @returns the coffee bean as an Observable
+   */
+  public getById(id: string): Observable<CoffeeBeanDto> {
+    return this.http.get<CoffeeBeanDto>('coffee-bean/' + id);
+  }
+}
