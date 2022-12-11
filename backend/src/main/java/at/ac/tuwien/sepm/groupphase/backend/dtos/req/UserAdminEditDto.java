552e95b09c25543731a00cc864c16c67a417b5cb
Patrick Cipot <patrick.cipot@gmail.com>
2022-12-11 20:26:58 +0100

    (account-management) [#21] Created UserAdminEditDto

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/dtos/req/UserAdminEditDto.java
@@ -0,0 +1,33 @@
+package at.ac.tuwien.sepm.groupphase.backend.dtos.req;
+
+import at.ac.tuwien.sepm.groupphase.backend.enums.UserRole;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+public class UserAdminEditDto {
+    @NotNull
+    private UserRole role;
+    @NotNull
+    private Boolean active;
+
+    public UserAdminEditDto() { }
+
+    public UserAdminEditDto(UserRole role, Boolean active) {
+        this.role = role;
+        this.active = active;
+    }
+
+    public UserRole getRole() {
+        return this.role;
+    }
+
+    public Boolean isActive() {
+        return this.active;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("UserAdminEditDto{role='%s',active=%s}", role, active);
+    }
+}
