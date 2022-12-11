0c47041fcedb00eb6c3442c9aa4d19499c8aa961
Jakob Novotny <e11800752@student.tuwien.ac.at>
2022-12-11 12:28:55 +0100

    (edit-account-data): added route for account data editing page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { TestHomeComponent } from './pages/test-home/test-home.component';
 import { AuthGuard } from '../services/auth/auth.guard';
 import { TestAdminPageComponent } from './pages/test-admin-page/test-admin-page.component';
+import { EditAccountDataComponent } from './pages/edit-account-data/edit-account-data.component';
 
 import type { Routes } from '@angular/router';
 
@@ -26,6 +27,7 @@ const routes: Routes = [
   },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'change', component: EditAccountDataComponent },
   { path: '**', redirectTo: 'home' },
 ];
 
