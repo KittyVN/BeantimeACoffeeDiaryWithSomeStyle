7fea6496008c9b27033eef117173ebf4a5d33e04
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2022-12-12 13:56:17 +0100

    (account-management) [#21] Implemented Error 404 handling for non-existent users.

--- frontend/src/app/pages/user-list/user-detail/user-detail.component.ts
+++ frontend/src/app/pages/user-list/user-detail/user-detail.component.ts
@@ -1,17 +1,17 @@
-import {Component, OnInit} from '@angular/core';
-import {UserService} from "../../../../services/user.service";
-import {ActivatedRoute, Router} from "@angular/router";
-import {UserDetailDto} from "../../../../dtos/req/userDetail.dto";
-import {UserAdminEditDto} from "../../../../dtos/req/userAdminEdit.dto";
-import {MatSnackBar} from "@angular/material/snack-bar";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { UserService } from '../../../../services/user.service';
+import { UserDetailDto } from '../../../../dtos/req/userDetail.dto';
+import { UserAdminEditDto } from '../../../../dtos/req/userAdminEdit.dto';
 
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.css']
+  styleUrls: ['./user-detail.component.css'],
 })
 export class UserDetailComponent implements OnInit {
-
   user: UserDetailDto = {
     id: 0,
     email: '',
@@ -24,28 +24,37 @@ export class UserDetailComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute,
     private snackBar: MatSnackBar
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(({id}) => {
+    this.route.params.subscribe(({ id }) => {
       this.service.getById(id).subscribe({
         next: data => {
           this.user = data;
-        }
-      })
+        },
+        error: error => {
+          if (error.status == 404) {
+            this.router.navigate(['/admin/users']);
+            this.snackBar.open(`User with ID ${this.user.id} not found.`, 'OK');
+          }
+        },
+      });
     });
   }
 
   onSubmit(): void {
     let userDto: UserAdminEditDto = {
       role: this.user.role,
-      active: this.user.active
-    }
+      active: this.user.active,
+    };
     this.service.updateByAdmin(this.user.id, userDto).subscribe({
       next: data => {
         this.user = data;
-        this.snackBar.open(`User ${this.user.email} successfully updated!`, 'OK');
-      }
+        this.snackBar.open(
+          `User ${this.user.email} successfully updated!`,
+          'OK'
+        );
+      },
     });
   }
 }
