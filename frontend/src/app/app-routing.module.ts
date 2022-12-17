ad15feb9570479e847a26760424881ae9ec7c057
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2022-12-17 23:40:23 +0100

    (coffee-detail-page) [#43] Added route to CoffeeBeanDetailComponent

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -18,6 +18,7 @@ import { UserListComponent } from './pages/user-list/user-list.component';
 import { CoffeeBeanDeleteComponent } from './pages/coffee-bean-delete/coffee-bean-delete.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { UserDetailComponent } from './pages/user-list/user-detail/user-detail.component';
+import { CoffeeBeanDetailComponent } from './pages/coffee-bean-detail/coffee-bean-detail.component';
 
 import type { Routes } from '@angular/router';
 
@@ -55,6 +56,10 @@ const routes: Routes = [
     path: 'profile',
     canActivate: [AuthGuard],
     component: ProfilePageComponent,
+  },
+  {
+    path: 'coffee/:id',
+    component: CoffeeBeanDetailComponent,
   },
   {
     path: 'create-coffee-bean',
