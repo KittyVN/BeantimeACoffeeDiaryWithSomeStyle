35bd7cbd045f3b0eb66e809f68134c66179ee30e
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2022-12-17 23:41:46 +0100

    (coffee-detail-page) [#43] Fetches coffee dto from server now

--- frontend/src/app/pages/coffee-bean-detail/coffee-bean-detail.component.ts
+++ frontend/src/app/pages/coffee-bean-detail/coffee-bean-detail.component.ts
@@ -1,8 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { CoffeeBeanDto } from '../../../dtos';
+import { Roast } from '../../../dtos/req/roast-type.enum';
+import { CoffeeBeanService } from '../../../services/coffee-bean.service';
 
 @Component({
   selector: 'app-coffee-bean-detail',
   templateUrl: './coffee-bean-detail.component.html',
   styleUrls: ['./coffee-bean-detail.component.css'],
 })
-export class CoffeeBeanDetailComponent {}
+export class CoffeeBeanDetailComponent implements OnInit {
+  coffee: CoffeeBeanDto = {
+    id: 0,
+    name: '',
+    price: undefined,
+    origin: undefined,
+    height: undefined,
+    coffeeRoast: Roast.LIGHT,
+    description: undefined,
+    custom: false,
+    userId: 0,
+  };
+
+  constructor(
+    private service: CoffeeBeanService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(({ id }) => {
+      this.service.getById(id).subscribe({
+        next: data => {
+          this.coffee = data;
+          console.log(this.coffee);
+        },
+        error: error => {
+          if (error.status == 404) {
+            // TODO: Error handling
+          }
+        },
+      });
+    });
+  }
+}
