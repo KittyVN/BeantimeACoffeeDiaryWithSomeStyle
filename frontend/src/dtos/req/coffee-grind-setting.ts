e7029d5c4509158c6c23bbaa3eebe47b83f012e7
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2022-12-17 23:27:42 +0100

    (coffee-detail-page) [#43] Created CoffeeGrindSettings enum in frontend

---
+++ frontend/src/dtos/req/coffee-grind-setting.ts
@@ -0,0 +1,8 @@
+export enum CoffeeGrindSetting {
+  coarse = 'COARSE',
+  mediumCoarse = 'MEDIUM_COARSE',
+  medium = 'MEDIUM',
+  mediumFine = 'MEDIUM_FINE',
+  fine = 'FINE',
+  extraFine = 'EXTRA_FINE',
+}
