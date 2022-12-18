4821f0a627d8631f1486a07e1100d79601164e0e
Daniel Schermann <e11777772@student.tuwien.ac.at>
2022-12-18 15:15:09 +0100

    [refactor/develop] fixed user delete

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/CoffeeBeanRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/CoffeeBeanRepository.java
@@ -1,10 +1,17 @@
 package at.ac.tuwien.sepm.groupphase.backend.repository;
 
 import at.ac.tuwien.sepm.groupphase.backend.entity.CoffeeBean;
-import at.ac.tuwien.sepm.groupphase.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CoffeeBeanRepository extends JpaRepository<CoffeeBean, Long> {
+    @Modifying
+    @Transactional
+    @Query("delete from CoffeeBean c where c.user.id = :id")
+    void deleteCoffeeBeanById(@Param("id") Long id);
 }
