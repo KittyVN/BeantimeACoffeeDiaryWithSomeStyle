45134a73f1c1c6703c1a42a83b59e5b8ed871118
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2022-12-18 14:47:22 +0100

    (coffee-avg-extraction-evaluation) [#46] Fixed copy-paste mishap

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/ExtractionRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/ExtractionRepository.java
@@ -1,7 +1,7 @@
 package at.ac.tuwien.sepm.groupphase.backend.repository;
 
-import at.ac.tuwien.sepm.groupphase.backend.entity.User;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Extraction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExtractionRepository extends JpaRepository<User, Long> {
+public interface ExtractionRepository extends JpaRepository<Extraction, Long> {
 }
