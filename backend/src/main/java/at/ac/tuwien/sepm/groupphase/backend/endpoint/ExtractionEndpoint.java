0e5b8ca2f4f473bf98ba8b70e4508f587cc72b25
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2022-12-19 18:16:51 +0100

    (coffee-avg-extraction-evaluation) [#46] Removed getAvgExtractionRatingByCoffeeBeanId

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/ExtractionEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/ExtractionEndpoint.java
@@ -1,15 +1,11 @@
 package at.ac.tuwien.sepm.groupphase.backend.endpoint;
 
-import at.ac.tuwien.sepm.groupphase.backend.dtos.req.CoffeeBeanAvgExtractionRating;
 import at.ac.tuwien.sepm.groupphase.backend.service.ExtractionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.security.PermitAll;
 import java.lang.invoke.MethodHandles;
 
 @RestController
@@ -21,12 +17,5 @@ public class ExtractionEndpoint {
 
     public ExtractionEndpoint(ExtractionService service) {
         this.service = service;
-    }
-
-    @PermitAll
-    @GetMapping("avg/{id}")
-    public CoffeeBeanAvgExtractionRating getAvgExtractionRatingByCoffeeBeanId(@PathVariable Long id) {
-        LOGGER.info(String.format("GET %s/avg/%d", BASE_PATH, id));
-        return this.service.getAvgExtractionEvaluationParamsByCoffeeBeanId(id);
     }
 }
