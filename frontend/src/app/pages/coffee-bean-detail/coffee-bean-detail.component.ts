94c0a77234feba15417314e61f8f9e649343a6e3
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2022-12-19 10:36:01 +0100

    (coffee-avg-extraction-evaluation) [#46] Added radar chart

--- frontend/src/app/pages/coffee-bean-detail/coffee-bean-detail.component.ts
+++ frontend/src/app/pages/coffee-bean-detail/coffee-bean-detail.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ChartData, ChartOptions, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 import { CoffeeBeanDto } from '../../../dtos';
 import { Roast } from '../../../dtos/req/roast-type.enum';
 import { CoffeeBeanService } from '../../../services/coffee-bean.service';
+import { CoffeeBeanAvgExtractionRating } from '../../../dtos/req/coffee-bean-avg-extraction-rating';
 
 @Component({
   selector: 'app-coffee-bean-detail',
@@ -24,6 +27,35 @@ export class CoffeeBeanDetailComponent implements OnInit {
     userId: 0,
   };
 
+  avgExtractionResults: CoffeeBeanAvgExtractionRating =
+    new CoffeeBeanAvgExtractionRating(0, 0, 0, 0, 0, 0);
+
+  radarChartLabels: string[] = [
+    'Body',
+    'Acidity',
+    'Aromatics',
+    'Sweetness',
+    'Aftertaste',
+  ];
+
+  radarChartOptions: ChartOptions<'radar'> = {
+    scales: {
+      r: {
+        suggestedMin: 0,
+        suggestedMax: 5,
+      },
+    },
+  };
+
+  radarChartType: ChartType = 'radar';
+
+  radarChartData: ChartData<'radar'> = {
+    labels: this.radarChartLabels,
+    datasets: [],
+  };
+
+  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
+
   constructor(
     private service: CoffeeBeanService,
     private router: Router,
@@ -34,6 +66,8 @@ export class CoffeeBeanDetailComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.subscribe(({ id }) => {
       this.coffee.id = id;
+      this.avgExtractionResults.id = id;
+
       this.service.getById(id).subscribe({
         next: data => {
           this.coffee = data;
@@ -46,6 +80,29 @@ export class CoffeeBeanDetailComponent implements OnInit {
               'OK'
             );
           }
+        },
+      });
+
+      this.service.getAvgExtractionResultsById(id).subscribe({
+        next: data => {
+          this.avgExtractionResults = new CoffeeBeanAvgExtractionRating(
+            data.id,
+            data.body,
+            data.acidity,
+            data.aromatics,
+            data.sweetness,
+            data.aftertaste
+          );
+
+          console.log(this.avgExtractionResults);
+          this.radarChartData.datasets = [
+            {
+              data: this.avgExtractionResults.getChartData(),
+              label: 'Average Extraction Results',
+            },
+          ];
+
+          this.chart?.update();
         },
       });
     });
