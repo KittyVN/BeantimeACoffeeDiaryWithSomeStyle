8f75711d32db158795607845ae3bdced80a6ed8a
Daniel Schermann <e11777772@student.tuwien.ac.at>
2022-12-20 17:27:33 +0100

    [feature/dashboard-sorting-searching] added extraction getAllByBean Test

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/ExtractionEndpointTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/ExtractionEndpointTest.java
@@ -1,4 +1,68 @@
 package at.ac.tuwien.sepm.groupphase.backend.endpoint;
 
+import at.ac.tuwien.sepm.groupphase.backend.dtos.req.ExtractionDetailDto;
+import at.ac.tuwien.sepm.groupphase.backend.dtos.req.UserDetailDto;
+import at.ac.tuwien.sepm.groupphase.backend.enums.UserRole;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles({"test", "generateData"})
+// enable "test" spring profile during test execution in order to pick up configuration from application-test.yml
+@SpringBootTest
+@EnableWebMvc
+@WebAppConfiguration
 public class ExtractionEndpointTest {
+    @Autowired
+    private WebApplicationContext webAppContext;
+    private MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+    }
+
+    @Test
+    @Transactional
+    @WithMockUser(username = "admin@example.com", password = "password", roles = "ADMIN")
+    public void getAllExtractionsByUserIdReturnsAllExtractions() throws Exception {
+        byte[] body = mockMvc
+            .perform(MockMvcRequestBuilders
+                .get("/api/v1/extractions/bean/2")
+                .accept(MediaType.APPLICATION_JSON)
+            ).andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsByteArray();
+
+        List<ExtractionDetailDto> userResult = objectMapper.readerFor(ExtractionDetailDto.class).<ExtractionDetailDto>readValues(body).readAll();
+
+        assertThat(userResult.size()).isGreaterThanOrEqualTo(6);
+        assertThat(userResult)
+            .map(ExtractionDetailDto::getId, ExtractionDetailDto::getDose, ExtractionDetailDto::getOverallRating)
+            .contains(tuple(1L, 200.0, 25))
+            .contains(tuple(2L, 200.0, 15))
+            .contains(tuple(3L, 200.0, 15))
+            .contains(tuple(4L, 200.0, 13))
+            .contains(tuple(5L, 200.0, 23))
+            .contains(tuple(6L, 200.0, 10));
+    }
 }
