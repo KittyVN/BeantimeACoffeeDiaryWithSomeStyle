1290fdad45159dd3dbabf6c2e53c5d602311eaed
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2022-12-20 17:02:48 +0100

    (coffee-avg-extraction-evaluation) Added 2 unit tests

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/CoffeeBeanEndpointTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/CoffeeBeanEndpointTest.java
@@ -1,7 +1,7 @@
 package at.ac.tuwien.sepm.groupphase.backend.endpoint;
 
+import at.ac.tuwien.sepm.groupphase.backend.dtos.req.CoffeeBeanDetailDto;
 import at.ac.tuwien.sepm.groupphase.backend.dtos.req.CoffeeBeanDto;
-import at.ac.tuwien.sepm.groupphase.backend.entity.CoffeeBean;
 import at.ac.tuwien.sepm.groupphase.backend.enums.CoffeeRoast;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,12 +15,11 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-
-import javax.transaction.Transactional;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -34,8 +33,9 @@ public class CoffeeBeanEndpointTest {
     @Autowired
     private WebApplicationContext webAppContext;
     private MockMvc mockMvc;
-
     private CoffeeBeanDto requestJson;
+    @Autowired
+    ObjectMapper objectMapper;
 
     @BeforeEach
     public void setup() {
@@ -169,5 +169,42 @@ public class CoffeeBeanEndpointTest {
         mockMvc.perform(MockMvcRequestBuilders
             .delete("/api/v1/coffee-beans/-9999")
         ).andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Transactional
+    public void getCoffeeBeanByExistentIdReturnsCoffeeBeanDetail() throws Exception {
+        byte[] body = mockMvc.perform(MockMvcRequestBuilders
+            .get("/api/v1/coffee-beans/2")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsByteArray();
+
+        CoffeeBeanDetailDto coffeeBeanResult = objectMapper.readerFor(CoffeeBeanDetailDto.class).<CoffeeBeanDetailDto>readValues(body).readAll().get(0);
+
+        assertThat(coffeeBeanResult).isNotNull();
+        assertThat(coffeeBeanResult.getId()).isEqualTo(2L);
+        assertThat(coffeeBeanResult.getCoffeeBean()).isNotNull();
+        assertThat(coffeeBeanResult.getCoffeeBean().getName()).isEqualTo("Another coffee bean");
+        assertThat(coffeeBeanResult.getCoffeeBean().getPrice()).isEqualTo(0F);
+        assertThat(coffeeBeanResult.getCoffeeBean().getOrigin()).isEqualTo("There");
+        assertThat(coffeeBeanResult.getCoffeeBean().getHeight()).isEqualTo(5);
+        assertThat(coffeeBeanResult.getCoffeeBean().getCoffeeRoast()).isEqualTo(CoffeeRoast.LIGHT);
+        assertThat(coffeeBeanResult.getCoffeeBean().getCustom()).isTrue();
+        assertThat(coffeeBeanResult.getCoffeeBean().getUserId()).isEqualTo(1L);
+        assertThat(coffeeBeanResult.getAvgExtractionRating()).isNotNull();
+        assertThat(coffeeBeanResult.getAvgExtractionRating().getId()).isEqualTo(2L);
+        assertThat(coffeeBeanResult.getAvgExtractionRating().getId()).isEqualTo(2L);
+        assertThat(coffeeBeanResult.getAvgExtractionRating().getAcidity()).isEqualTo(3.3333333333333335);
+        assertThat(coffeeBeanResult.getAvgExtractionRating().getAftertaste()).isEqualTo(3.5);
+        assertThat(coffeeBeanResult.getAvgExtractionRating().getAromatics()).isEqualTo(3.3333333333333335);
+        assertThat(coffeeBeanResult.getAvgExtractionRating().getBody()).isEqualTo(2.8333333333333335);
+        assertThat(coffeeBeanResult.getAvgExtractionRating().getSweetness()).isEqualTo(3.8333333333333335);
+    }
+
+    @Test
+    @Transactional
+    public void getCoffeeBeanByNonExistentIdReturns404() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/coffee-beans/0")).andExpect(status().isNotFound());
     }
 }
