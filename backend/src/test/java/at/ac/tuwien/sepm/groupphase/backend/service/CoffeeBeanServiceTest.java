46f2a952d3564031c554ad4b545568a3dc7a9dd7
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2022-12-20 17:04:01 +0100

    (coffee-avg-extraction-evaluation) Added 2 unit tests

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/service/CoffeeBeanServiceTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/service/CoffeeBeanServiceTest.java
@@ -2,16 +2,13 @@ package at.ac.tuwien.sepm.groupphase.backend.service;
 
 import at.ac.tuwien.sepm.groupphase.backend.dtos.req.CoffeeBeanDto;
 import at.ac.tuwien.sepm.groupphase.backend.enums.CoffeeRoast;
+import at.ac.tuwien.sepm.groupphase.backend.exception.NotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
-
-
-import javax.transaction.Transactional;
-
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -71,4 +68,28 @@ public class CoffeeBeanServiceTest {
         assertDoesNotThrow(() -> coffeeBeanService.delete(1L));
     }
 
+    @Test
+    @Transactional
+    public void getCoffeeBeanByExistentIdReturnsCoffeeBeanDTO() {
+        CoffeeBeanDto result = coffeeBeanService.getById(2L);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getName()).isEqualTo("Another coffee bean");
+        assertThat(result.getPrice()).isEqualTo(0F);
+        assertThat(result.getOrigin()).isEqualTo("There");
+        assertThat(result.getHeight()).isEqualTo(5);
+        assertThat(result.getCoffeeRoast()).isEqualTo(CoffeeRoast.LIGHT);
+        assertThat(result.getCustom()).isTrue();
+        assertThat(result.getUserId()).isEqualTo(1L);
+    }
+
+    @Test
+    @Transactional
+    public void getCoffeeBeanByNonExistentIdReturnsNotFoundException() {
+        try {
+            coffeeBeanService.getById(0L);
+        } catch (Exception e) {
+            assertThat(e instanceof NotFoundException);
+        }
+    }
 }
