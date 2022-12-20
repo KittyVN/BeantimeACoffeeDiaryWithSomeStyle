affd10f1cd5b461190bf2bb16aedd4242817ceba
Daniel Schermann <e11777772@student.tuwien.ac.at>
2022-12-20 17:16:34 +0100

    [feature/dashboard-sorting-searching] fixed extraction getAllByBean

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/ExtractionServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/ExtractionServiceImpl.java
@@ -4,6 +4,7 @@ import at.ac.tuwien.sepm.groupphase.backend.dtos.req.CoffeeBeanAvgExtractionRati
 import at.ac.tuwien.sepm.groupphase.backend.dtos.req.ExtractionDetailDto;
 import at.ac.tuwien.sepm.groupphase.backend.exception.NotFoundException;
 import at.ac.tuwien.sepm.groupphase.backend.mapper.ExtractionMapper;
+import at.ac.tuwien.sepm.groupphase.backend.repository.CoffeeBeanRepository;
 import at.ac.tuwien.sepm.groupphase.backend.repository.ExtractionRepository;
 import at.ac.tuwien.sepm.groupphase.backend.repository.UserRepository;
 import at.ac.tuwien.sepm.groupphase.backend.service.ExtractionService;
@@ -19,14 +20,15 @@ import java.util.stream.Stream;
 public class ExtractionServiceImpl implements ExtractionService {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private final ExtractionRepository extractionRepository;
-    private final UserRepository userRepository;
+    private final CoffeeBeanRepository beanRepository;
+
     private final ExtractionMapper mapper;
 
 
     @Autowired
-    public ExtractionServiceImpl(ExtractionRepository extractionRepository, UserRepository userRepository, ExtractionMapper mapper) {
+    public ExtractionServiceImpl(ExtractionRepository extractionRepository, CoffeeBeanRepository beanRepository, ExtractionMapper mapper) {
         this.extractionRepository = extractionRepository;
-        this.userRepository = userRepository;
+        this.beanRepository = beanRepository;
         this.mapper = mapper;
     }
 
@@ -42,10 +44,10 @@ public class ExtractionServiceImpl implements ExtractionService {
     }
 
     @Override
-    public Stream<ExtractionDetailDto> getAllByUserId(Long id) {
-        LOGGER.trace("getAllByUserId({})", id);
-        if (userRepository.existsById(id)) {
-            return extractionRepository.findAllByUserId(id).stream().map(extraction -> mapper.entityToDto(extraction));
+    public Stream<ExtractionDetailDto> getAllByBeanId(Long id) {
+        LOGGER.trace("getAllByBeanId({})", id);
+        if (beanRepository.existsById(id)) {
+            return extractionRepository.findAllByBeanId(id).stream().map(extraction -> mapper.entityToDto(extraction));
         } else {
             throw new NotFoundException(String.format("No user with ID %d found", id));
         }
