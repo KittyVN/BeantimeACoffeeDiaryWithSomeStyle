fc27f2121eb7717363885d55df9b86974e55743b
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2023-01-12 18:39:36 +0100

    (profile-page) [#58] Removed outsourced code

--- frontend/src/app/pages/profile-page/profile-page.component.ts
+++ frontend/src/app/pages/profile-page/profile-page.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
-import { DeleteDialogComponent } from 'src/app/components/dialog/delete-dialog/delete-dialog.component';
 import { UserService } from 'src/services/user.service';
 
 @Component({
@@ -11,22 +8,5 @@ import { UserService } from 'src/services/user.service';
   styleUrls: ['./profile-page.component.css'],
 })
 export class ProfilePageComponent {
-  constructor(
-    public dialog: MatDialog,
-    private router: Router,
-    private userService: UserService
-  ) {}
-
-  openDialog(): void {
-    const dialogRef = this.dialog.open(DeleteDialogComponent, {
-      width: '300px',
-      hasBackdrop: true,
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result == 'closed') {
-        this.router.navigate(['/login']);
-      }
-    });
-  }
+  constructor(private router: Router, private userService: UserService) {}
 }
