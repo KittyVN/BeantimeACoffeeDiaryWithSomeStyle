c281bb247ff5af344ddc6641079aab9169ce375a
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2023-01-12 18:53:14 +0100

    (profile-page) [#58] Implemented account toolbar

--- frontend/src/app/pages/profile-page/profile-page.component.ts
+++ frontend/src/app/pages/profile-page/profile-page.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from 'src/services/user.service';
+
+import { UserDetailDto } from '../../../dtos';
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.css'],
 })
-export class ProfilePageComponent {
+export class ProfilePageComponent implements OnInit {
   constructor(private router: Router, private userService: UserService) {}
+
+  userId: number = this.userService.thisUserId();
+  user: UserDetailDto = {
+    id: 0,
+    email: 'email@example.com',
+    role: 'USER',
+    active: true,
+  };
+
+  ngOnInit(): void {
+    this.userService.getSelf(this.userId).subscribe({
+      next: user => {
+        this.user = user;
+      },
+    });
+  }
 }
