36c3b216b8f3be34329504c08c352dab8876d9ce
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2023-01-13 15:36:01 +0100

    (profile-page) [#58] Implemented month labels

--- frontend/src/app/pages/profile-page/profile-page.component.ts
+++ frontend/src/app/pages/profile-page/profile-page.component.ts
@@ -17,11 +17,36 @@ export class ProfilePageComponent implements OnInit {
     email: 'Loading ...',
     extractionMatrix: undefined,
   };
+  extractionMatrixMonthLabels: Map<number, string> = new Map<number, string>();
+
+  today = new Date();
+  startDate = new Date();
 
   ngOnInit(): void {
+    this.startDate.setDate(
+      this.today.getDate() - ((this.today.getDay() + 6) % 7) - 53 * 7
+    );
+
+    let i: number = 2;
+    let currentMonth: number = -1;
+    let date = new Date(this.startDate.valueOf());
+    while (date < this.today) {
+      if (date.getMonth() !== currentMonth) {
+        this.extractionMatrixMonthLabels.set(
+          i,
+          date.toLocaleDateString('en', { month: 'short' })
+        );
+        currentMonth = date.getMonth();
+      }
+
+      date.setDate(date.getDate() + 7);
+      i++;
+    }
+
     this.userService.getProfileById(this.userId).subscribe({
       next: profile => {
         this.profile = profile;
+        console.log(this.profile);
       },
     });
   }
