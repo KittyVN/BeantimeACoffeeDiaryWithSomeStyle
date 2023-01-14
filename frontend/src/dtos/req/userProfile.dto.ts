b802cf9ec447644216ecd5c6e12dd676525c0f8d
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2023-01-14 13:47:29 +0100

    (profile-page) [#58] Added topMostExtractedCoffees

--- frontend/src/dtos/req/userProfile.dto.ts
+++ frontend/src/dtos/req/userProfile.dto.ts
@@ -1,8 +1,10 @@
 import { ExtractionMatrixDto } from './extractionMatrix.dto';
 import { ExtractionListDto } from './extractionList.dto';
+import { CoffeeBeanExtractionsListDto } from './CoffeeBeanExtractionsList.dto';
 
 export interface UserProfileDto {
   email: string;
   extractionMatrix?: ExtractionMatrixDto;
   topRatedExtractions?: ExtractionListDto[];
+  topMostExtractedCoffees?: CoffeeBeanExtractionsListDto[];
 }
