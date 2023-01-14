645043544c73c7b43360d65b9a15ecf9010ebf4f
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2023-01-14 11:32:28 +0100

    (profile-page) [#58] Added topRatedExtractions

--- frontend/src/dtos/req/userProfile.dto.ts
+++ frontend/src/dtos/req/userProfile.dto.ts
@@ -1,6 +1,8 @@
 import { ExtractionMatrixDto } from './extractionMatrix.dto';
+import { ExtractionListDto } from './extractionList.dto';
 
 export interface UserProfileDto {
   email: string;
   extractionMatrix?: ExtractionMatrixDto;
+  topRatedExtractions?: ExtractionListDto[];
 }
