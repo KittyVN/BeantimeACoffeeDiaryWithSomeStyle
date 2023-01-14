1eb59a71739c46b07abc316132b507f5299e6ca6
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2023-01-14 13:41:56 +0100

    (profile-page) [#58] Implemented findTop10ExtractedByUserId()

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/CoffeeBeanRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/CoffeeBeanRepository.java
@@ -8,7 +8,9 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.Tuple;
 import java.util.Collection;
+import java.util.List;
 
 @Repository
 public interface CoffeeBeanRepository extends JpaRepository<CoffeeBean, Long>, CoffeeBeanRepositoryCustom {
@@ -19,4 +21,15 @@ public interface CoffeeBeanRepository extends JpaRepository<CoffeeBean, Long>, C
 
     @Query("select c from CoffeeBean c where c.user.id = :id")
     Collection<CoffeeBean> findAllByUser(@Param("id") Long id);
+
+    /**
+     * Find the 10 top most extracted coffees of a specific user.
+     *
+     * @param id of the user
+     * @return a List of Tuples
+     */
+    @Query(value = "SELECT b.ID, b.NAME, COUNT(e.ID) AS numExtractions FROM COFFEE_EXTRACTION e "
+        + "JOIN COFFEE_BEAN b on e.COFFEE_BEAN_ID = b.ID WHERE b.USER_ID = 1 "
+        + "GROUP BY b.ID ORDER BY numExtractions DESC, b.NAME LIMIT 10", nativeQuery = true)
+    List<Tuple> findTop10ExtractedByUserId(@Param("id") Long id);
 }
