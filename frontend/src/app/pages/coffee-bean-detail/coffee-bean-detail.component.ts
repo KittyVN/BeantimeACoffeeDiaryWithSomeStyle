a778c657c8dec534f1498a55fc88415bceab66c1
CelLumi <florian.stolz@hotmail.com>
2023-01-15 16:11:07 +0100

    (export-excel-worksheet) Added the feature to export the extractions of a coffee bean into an excel sheet

--- frontend/src/app/pages/coffee-bean-detail/coffee-bean-detail.component.ts
+++ frontend/src/app/pages/coffee-bean-detail/coffee-bean-detail.component.ts
@@ -6,6 +6,8 @@ import { ChartData, ChartOptions, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import { ExtractionSearchDto } from 'src/dtos/req/extraction-search-dto';
 import { debounce, interval, scan, Subject } from 'rxjs';
+import { Workbook } from 'exceljs';
+import { saveAs } from 'file-saver';
 
 import { CoffeeBeanDto, ExtractionDetailDto } from '../../../dtos';
 import { Roast } from '../../../dtos/req/roast-type.enum';
@@ -173,5 +175,81 @@ export class CoffeeBeanDetailComponent implements OnInit {
             },
           });
       });
+  }
+
+  excelExport() {
+    let workbook = new Workbook();
+    let datamap = workbook.addWorksheet('Extractions');
+
+    datamap.columns = [
+      { header: 'Created at', key: 'created', width: 24 },
+      { header: 'Grind setting', key: 'grind', width: 14 },
+      { header: 'Brew method', key: 'brew', width: 20 },
+      { header: 'Water temperature', key: 'temp', width: 20 },
+      { header: 'Water amount', key: 'water', width: 16 },
+      { header: 'Dose', key: 'dose', width: 8 },
+      { header: 'Brew time', key: 'time', width: 12 },
+      { header: ' ', key: 'gap', width: 4 },
+      { header: 'Overall Rating', key: 'rating', width: 16 },
+      { header: 'Body', key: 'body', width: 6 },
+      { header: 'Acidity', key: 'acidity', width: 8 },
+      { header: 'Aromatics', key: 'aromatics', width: 12 },
+      { header: 'Sweetness', key: 'sweetness', width: 12 },
+      { header: 'Aftertaste', key: 'aftertaste', width: 10 },
+      { header: ' ', key: 'gap2', width: 4 },
+      { header: 'Rating notes', key: 'notes', width: 60 },
+    ];
+
+    datamap.getRow(1).font = { bold: true };
+    datamap.getRow(1).alignment = { horizontal: 'center' };
+
+    if (this.extractions.length > 0) {
+      this.extractions.forEach(ex => {
+        datamap.addRow({
+          created: ex.dateTime,
+          grind: ex.grindSetting,
+          brew: ex.brewMethod,
+          temp: ex.waterTemperature,
+          water: ex.waterAmount,
+          dose: ex.dose,
+          time: this.formatBrewTime(ex),
+          gap: '',
+          rating: ex.overallRating,
+          body: ex.body,
+          acidity: ex.acidity,
+          aromatics: ex.aromatics,
+          sweetness: ex.sweetness,
+          aftertaste: ex.aftertaste,
+          gap2: '',
+          notes: ex.ratingNotes,
+        });
+      });
+
+      workbook.xlsx.writeBuffer().then(extractions => {
+        let blob = new Blob([extractions], {
+          type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        });
+        saveAs(blob, 'Extractions_' + this.coffee.name + '.xlsx');
+      });
+    }
+  }
+
+  formatBrewTime(ex: ExtractionDetailDto): string {
+    if (ex) {
+      let brewtime = ex.brewTime;
+      let milliseconds = Math.floor((brewtime % 1000) / 100),
+        seconds = Math.floor((brewtime / 1000) % 60),
+        minutes = Math.floor((brewtime / (1000 * 60)) % 60),
+        hours = Math.floor((brewtime / (1000 * 60 * 60)) % 24);
+
+      return (
+        (hours < 10 ? '0' + hours : hours) +
+        ':' +
+        (minutes < 10 ? '0' + minutes : minutes) +
+        ':' +
+        (seconds < 10 ? '0' + seconds : seconds)
+      );
+    }
+    return '???';
   }
 }
