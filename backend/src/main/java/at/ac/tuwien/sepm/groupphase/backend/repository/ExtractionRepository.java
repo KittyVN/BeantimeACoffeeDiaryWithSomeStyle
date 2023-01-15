790fe93f85ecbbebd4735af3a19d2166615d3324
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2023-01-15 14:34:03 +0100

    (profile-page) [#58] Same timestamp as in service

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/ExtractionRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/ExtractionRepository.java
@@ -46,7 +46,7 @@ public interface ExtractionRepository extends JpaRepository<Extraction, Long> {
      */
     @Query(value = "SELECT CAST(e.EXTRACTION_DATE as DATE), COUNT(e.ID), 0 FROM COFFEE_EXTRACTION e "
         + "JOIN COFFEE_BEAN b on e.COFFEE_BEAN_ID = b.ID JOIN APPLICATION_USER u on u.ID = b.USER_ID "
-        + "WHERE u.ID = :id AND e.EXTRACTION_DATE >= (CURRENT_DATE - (ISO_DAY_OF_WEEK(CURRENT_DATE) - 1) - 53 * 7) "
+        + "WHERE u.ID = :id AND e.EXTRACTION_DATE >= (CURRENT_DATE - (ISO_DAY_OF_WEEK(CURRENT_DATE) - 1) - 52 * 7) "
         + "GROUP BY e.EXTRACTION_DATE ORDER BY CAST(e.EXTRACTION_DATE as DATE)",
         nativeQuery = true)
     List<Tuple> findDailyCountsForLast53WeeksByUserId(@Param("id") Long id);
