a0921b7b7431d09c81ec27ed999f2fea5b69e277
Jakob Novotny <e11800752@student.tuwien.ac.at>
2023-01-19 13:35:07 +0100

    (refactor): added reddit logo to buttons)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitzer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'reddit_logo',
+      this.domSanitzer.bypassSecurityTrustResourceUrl(
+        './assets/reddit_logo.svg'
+      )
+    );
+  }
 }
--- frontend/src/util/api-interceptor.ts
+++ frontend/src/util/api-interceptor.ts
@@ -16,7 +16,8 @@ export class APIInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     if (
       req.url !== 'https://www.reddit.com/api/v1/access_token' &&
-      req.url !== 'https://oauth.reddit.com/api/submit'
+      req.url !== 'https://oauth.reddit.com/api/submit' &&
+      req.url !== './assets/reddit_logo.svg'
     ) {
       const apiReq = req.clone({
         url: `${environment.apiBase}/${req.url}`,
