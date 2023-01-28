ccd1cd3049c887f14f4792a9bc6d4789418a640a
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2023-01-29 00:54:06 +0100

    (feat/recipe-detail-page) [#62] Changed mehtod type to Collection

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/RecipeRatingRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/RecipeRatingRepository.java
@@ -3,8 +3,8 @@ package at.ac.tuwien.sepm.groupphase.backend.repository;
 import at.ac.tuwien.sepm.groupphase.backend.entity.RecipeRating;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Collection;
 
 public interface RecipeRatingRepository extends JpaRepository<RecipeRating, Long> {
-    List<RecipeRating> findRecipeRatingByRecipe_Id(long recipeId);
+    Collection<RecipeRating> findRecipeRatingByRecipe_Id(long recipeId);
 }
