877214d4efe938abccb29043164c1ae348c048a1
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2023-01-29 00:04:55 +0100

    (feat/username) [#62] Created RecipeRating

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/RecipeRating.java
@@ -0,0 +1,62 @@
+package at.ac.tuwien.sepm.groupphase.backend.entity;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "recipe_rating")
+public class RecipeRating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne()
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "recipe_id")
+    private Recipe recipe;
+
+    @ManyToOne()
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    @Column(name = "timestamp")
+    private LocalDateTime timestamp;
+
+    public RecipeRating() {
+    }
+
+    public RecipeRating(Long id, Recipe recipe, User author) {
+        this.id = id;
+        this.recipe = recipe;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+}
