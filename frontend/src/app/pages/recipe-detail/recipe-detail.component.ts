2f6a5304bea552a40ef26cc1c0957a5c0325ae4d
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2023-01-28 16:59:50 +0100

    (feat/recipe-detail-page) [#61] Added detail card

--- frontend/src/app/pages/recipe-detail/recipe-detail.component.ts
+++ frontend/src/app/pages/recipe-detail/recipe-detail.component.ts
@@ -3,6 +3,11 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router, ActivatedRoute } from '@angular/router';
 import { CommunityRecipeDto } from 'src/dtos/req/community-recipe.dto';
 import { RecipeService } from 'src/services/recipe.service';
+import { Observable } from 'rxjs';
+
+import { BrewMethod } from '../../../dtos/req/brew-method.enum';
+import { RecipeDto } from '../../../dtos/req/recipe.dto';
+import { RedditService } from '../../../services/reddit.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -44,7 +49,8 @@ export class RecipeDetailComponent implements OnInit {
     private recipeService: RecipeService,
     private router: Router,
     private route: ActivatedRoute,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private redditService: RedditService
   ) {}
 
   ngOnInit(): void {
@@ -67,6 +73,107 @@ export class RecipeDetailComponent implements OnInit {
           },
         });
       }
+    });
+  }
+
+  formatBrewMethod(recipe: CommunityRecipeDto): string {
+    switch (recipe.extractionBrewMethod) {
+      case BrewMethod.DRIP: {
+        return 'Drip';
+      }
+      case BrewMethod.BOILING: {
+        return 'Boiling';
+      }
+      case BrewMethod.STEEPING: {
+        return 'Steeping';
+      }
+      case BrewMethod.PRESSURE: {
+        return 'Pressure';
+      }
+      case BrewMethod.TURKISH: {
+        return 'Turkish';
+      }
+      case BrewMethod.FRENCH_PRESS: {
+        return 'French press';
+      }
+      case BrewMethod.COLD_BREW: {
+        return 'Cold Brew';
+      }
+      case BrewMethod.INSTANT: {
+        return 'Instant';
+      }
+      case BrewMethod.POUR_OVER: {
+        return 'Pour over';
+      }
+      case BrewMethod.V60: {
+        return 'v60';
+      }
+      case BrewMethod.ESPRESSO_MACHINE: {
+        return 'Espresso machine';
+      }
+      case BrewMethod.MOKA: {
+        return 'Moka';
+      }
+      case BrewMethod.AEROPRESS: {
+        return 'Aeropress';
+      }
+      case BrewMethod.POD_MACHINE: {
+        return 'Pod';
+      }
+      case BrewMethod.OTHER: {
+        return 'Other';
+      }
+      default: {
+        return 'Unknown brewing method';
+      }
+    }
+  }
+
+  deleteRecipe(): void {
+    this.recipeService.delete(this.recipe.recipeId.toString()).subscribe({
+      next: res => {
+        this.snackBar.open('Successfully deleted recipe', 'Close', {
+          duration: 5000,
+        });
+      },
+      error: err => {
+        this.snackBar.open(err.error.match('\\[.*?\\]'), 'Close', {
+          duration: 5000,
+        });
+      },
+    });
+  }
+
+  shareOnReddit() {
+    this.redditService.postToReddit(this.recipe);
+  }
+
+  editRecipe() {
+    let observable: Observable<string>;
+    let recipeDto: RecipeDto = {
+      id: this.recipe.recipeId,
+      shared: this.recipe.recipeShared,
+      extractionId: this.recipe.extractionId,
+    };
+
+    if (this.recipe.recipeShared) {
+      recipeDto.shared = false;
+    } else {
+      recipeDto.shared = true;
+    }
+    observable = this.recipeService.edit(recipeDto);
+
+    observable.subscribe({
+      next: data => {
+        this.recipe.recipeShared = recipeDto.shared;
+      },
+      error: err => {
+        if (err.status == 404) {
+          this.snackBar.open('There is no recipe by that id', 'Close', {
+            duration: 5000,
+          });
+        }
+      },
     });
   }
 }
