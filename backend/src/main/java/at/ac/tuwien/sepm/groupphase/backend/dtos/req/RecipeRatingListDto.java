970e4281e658de93b194375598e32bb749b06b60
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2023-01-29 00:13:32 +0100

    (feat/recipe-detail-page) [#62] Created RecipeRatingListDto

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/dtos/req/RecipeRatingListDto.java
@@ -0,0 +1,70 @@
+package at.ac.tuwien.sepm.groupphase.backend.dtos.req;
+
+import javax.validation.constraints.NotNull;
+
+public class RecipeRatingListDto {
+    @NotNull
+    private long id;
+
+    @NotNull
+    private long recipeId;
+
+    @NotNull
+    private long userId;
+
+    @NotNull
+    private int rating;
+
+    private String text;
+
+    public RecipeRatingListDto() {
+    }
+
+    public RecipeRatingListDto(long id, long recipeId, long userId, int rating, String text) {
+        this.id = id;
+        this.recipeId = recipeId;
+        this.userId = userId;
+        this.rating = rating;
+        this.text = text;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(long recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
