225fbd90bb298cca95621dc6c04419eee91896c3
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2023-01-29 10:02:25 +0100

    (feat/recipe-detail-page) [#62] Added pre-authorization

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/RecipeRatingEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/RecipeRatingEndpoint.java
@@ -6,7 +6,15 @@ import at.ac.tuwien.sepm.groupphase.backend.dtos.req.RecipeRatingUpdateDto;
 import at.ac.tuwien.sepm.groupphase.backend.service.RecipeRatingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.invoke.MethodHandles;
 import java.util.stream.Stream;
@@ -22,17 +30,21 @@ public class RecipeRatingEndpoint {
         this.service = service;
     }
 
+    @PreAuthorize("(hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')) ")
     @PostMapping("{recipe_id}/ratings")
     public RecipeRatingListDto create(@PathVariable("recipe_id") long recipeId,
                                       @RequestBody RecipeRatingCreateDto recipeRatingCreateDto) {
         return null;
     }
 
+    @PreAuthorize("(hasRole('ROLE_USER') or hasRole('ROLE_ADMIN'))")
     @GetMapping("{recipe_id}/ratings")
     public Stream<RecipeRatingListDto> getByRatingId(@PathVariable("recipe_id") long id) {
         return null;
     }
 
+    @PreAuthorize("(hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')) "
+        + "and authentication.principal.equals(recipeRatingUpdateDto.authorId)")
     @PutMapping("{recipe_id}/ratings/{id}")
     public RecipeRatingListDto update(@PathVariable("recipe_id") long recipeId,
                                       @PathVariable("id") long id,
@@ -40,10 +52,10 @@ public class RecipeRatingEndpoint {
         return null;
     }
 
+    @PreAuthorize("(hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')) "
+        + "and authentication.principal.equals(recipeRatingUpdateDto.authorId)")
     @DeleteMapping("{recipe_id}/ratings/{id}")
     public void delete(@PathVariable("recipe_id") long recipeId,
                        @PathVariable("id") long id) {
     }
-
-    // TODO: Preauthenticate
 }
