b6eea868d92226f9856365c3283d9760223dd71d
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2023-01-29 10:33:34 +0100

    (feat/recipe-detail-page) [#62] Implemented create

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/RecipeRatingEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/RecipeRatingEndpoint.java
@@ -3,18 +3,23 @@ package at.ac.tuwien.sepm.groupphase.backend.endpoint;
 import at.ac.tuwien.sepm.groupphase.backend.dtos.req.RecipeRatingCreateDto;
 import at.ac.tuwien.sepm.groupphase.backend.dtos.req.RecipeRatingListDto;
 import at.ac.tuwien.sepm.groupphase.backend.dtos.req.RecipeRatingUpdateDto;
+import at.ac.tuwien.sepm.groupphase.backend.exception.ConflictException;
 import at.ac.tuwien.sepm.groupphase.backend.service.RecipeRatingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.lang.invoke.MethodHandles;
 import java.util.stream.Stream;
 
 @RestController
 @RequestMapping(path = RecipeRatingEndpoint.BASE_PATH)
+@Validated
 public class RecipeRatingEndpoint {
     static final String BASE_PATH = "/api/v1/recipes";
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
@@ -24,11 +29,17 @@ public class RecipeRatingEndpoint {
         this.service = service;
     }
 
-    @PreAuthorize("(hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')) ")
+    @PreAuthorize("(hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')) "
+        + "and #recipeId.toString().equals(#recipeRatingCreateDto.getRecipeId().toString())")
     @PostMapping("{recipe_id}/ratings")
     public RecipeRatingListDto create(@PathVariable("recipe_id") long recipeId,
-                                      @RequestBody RecipeRatingCreateDto recipeRatingCreateDto) {
-        return null;
+                                      @Valid @RequestBody RecipeRatingCreateDto recipeRatingCreateDto) {
+        LOGGER.info("POST {}/{}/ratings with RequestBody: {}", BASE_PATH, recipeId, recipeRatingCreateDto);
+        try {
+            return service.create(recipeRatingCreateDto);
+        } catch (ConflictException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage(), e);
+        }
     }
 
     @PreAuthorize("(hasRole('ROLE_USER') or hasRole('ROLE_ADMIN'))")
