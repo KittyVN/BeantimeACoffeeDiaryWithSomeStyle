bc71a67c16a030a6a1e8f5ce1c13e768be9905b1
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2023-01-29 14:15:47 +0100

    (feat/recipe-detail-page) [#62] Implemented getByRecipeId

--- frontend/src/services/recipeRating.service.ts
+++ frontend/src/services/recipeRating.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { Observable } from 'rxjs';
 
 import { RecipeRatingCreateDto } from '../dtos/req/recipeRatingCreate.dto';
 import { RecipeRatingUpdateDto } from '../dtos/req/recipeRatingUpdate.dto';
+import { RecipeRatingListDto } from '../dtos/req/recipeRatingList.dto';
 
 @Injectable({ providedIn: 'root' })
 export class RecipeRatingService {
@@ -21,7 +23,11 @@ export class RecipeRatingService {
    *
    * @param recipeId of the recipe
    */
-  public getByRecipeId(recipeId: number) {}
+  public getByRecipeId(recipeId: number): Observable<RecipeRatingListDto[]> {
+    return this.http
+      .get<RecipeRatingListDto[]>(`recipes/${recipeId}/ratings`)
+      .pipe();
+  }
 
   /**
    * Update a rating with the given dto
