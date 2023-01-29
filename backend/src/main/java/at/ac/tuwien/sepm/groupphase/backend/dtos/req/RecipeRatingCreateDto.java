3dcfd5a8e3b1a24652cdae3c7e9e90cf324d20ac
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2023-01-29 10:32:46 +0100

    (feat/recipe-detail-page) [#62] Added max/min error messages

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/dtos/req/RecipeRatingCreateDto.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/dtos/req/RecipeRatingCreateDto.java
@@ -12,8 +12,8 @@ public class RecipeRatingCreateDto {
     private long userId;
 
     @NotNull
-    @Min(1)
-    @Max(5)
+    @Min(value = 1, message = "Rating must be in the range between 1 and 5")
+    @Max(value = 5, message = "Rating must be in the range between 1 and 5")
     private int rating;
 
     private String text;
