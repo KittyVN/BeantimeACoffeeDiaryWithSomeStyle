0670d126a79ebc8eaf2681b4a46f425514b49a8a
Patrick Cipot <patrick.cipot@student.tuwien.ac.at>
2023-01-31 12:37:26 +0100

    (feat/recipe-detail-page) [#62] Created get tests

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/service/RecipeRatingServiceTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/service/RecipeRatingServiceTest.java
@@ -8,21 +8,14 @@ import at.ac.tuwien.sepm.groupphase.backend.exception.NotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles({"test", "generateData"}) // enable "test" spring profile during test execution in order to pick up configuration from application-test.yml
 @SpringBootTest
@@ -88,4 +81,30 @@ public class RecipeRatingServiceTest {
     }
 
     /* GET tests */
+    @Test
+    @Transactional
+    public void getByExistentRatingIdReturnsStream() {
+        List<RecipeRatingListDto> ratingResult = service.getByRecipeId(1L).toList();
+
+        assertThat(ratingResult).isNotNull();
+        assertThat(ratingResult.size()).isEqualTo(6);
+        assertThat(ratingResult)
+            .map(RecipeRatingListDto::getId, RecipeRatingListDto::getRecipeId, RecipeRatingListDto::getAuthorId,
+                RecipeRatingListDto::getAuthorUsername, RecipeRatingListDto::getRating, RecipeRatingListDto::getText)
+            .contains(tuple(1L, 1L, 2L, "JohnD", 4, null))
+            .contains(tuple(2L, 1L, 4L, "Südfrau", 5, "Delicious!"))
+            .contains(tuple(3L, 1L, 6L, "Jane", 3, "Tried it for the first time and found out it's not good and not bad."))
+            .contains(tuple(4L, 1L, 8L, "Olaf*2", 4, null))
+            .contains(tuple(5L, 1L, 7L, "Jan", 1, "Seldom have I ever drank something so disgusting!"))
+            .contains(tuple(6L, 1L, 3L, "Musterf", 5, null));
+    }
+
+    @Test
+    @Transactional
+    public void getByNonExistentRatingIdReturnsEmptyStream() {
+        List<RecipeRatingListDto> ratingResult = service.getByRecipeId(0L).toList();
+
+        assertThat(ratingResult).isNotNull();
+        assertThat(ratingResult.size()).isEqualTo(0);
+    }
 }
